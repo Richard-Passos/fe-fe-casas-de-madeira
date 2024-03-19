6a143fbf3c0180a347300e9bc846552244b70141
Richard Passos <richard.ac.passos12@gmail.com>
2024-03-19 12:05:30 -0300

    Add ?

--- src/components/cookies-consent/Root.jsx
+++ src/components/cookies-consent/Root.jsx
@@ -3,7 +3,7 @@ import { toastsApi } from '@/api';
 import Toast from './Toast';
 
 const CookiesConsent = async (props) => {
-  const data = (await toastsApi.get('?populate=cookiesConsent')).data
+  const data = (await toastsApi.get('?populate=cookiesConsent'))?.data
     ?.cookiesConsent;
 
   return (
